ab68b1ec590b74a2962f8bcd7d456019dd6378ab
shero121 <keithsherwin6764@gmail.com>
2023-11-23 22:41:07 +0000

    when register you are now brought to the login page

--- register_process.php
+++ register_process.php
@@ -5,17 +5,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+
+
     $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Registration successful!";
+
+        header("Location: login.html");
+        exit();
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
     $conn->close();
 } else {
-    header("Location: register.php");
+    header("Location: register.html");
     exit();
 }
 ?>
