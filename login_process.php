b1294ad1410ad6383b4bb6642dc28cc34dc52f01
shero121 <keithsherwin6764@gmail.com>
2023-11-23 22:22:30 +0000

    the login page now works by connecting to the users db and only directing to the homepage once logged in

---
+++ login_process.php
@@ -0,0 +1,25 @@
+<?php
+date_default_timezone_set('GMT'); 
+session_start();
+
+include('dbcon.php');
+
+$enteredUsername = $_POST['username'];
+$enteredPassword = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username = '$enteredUsername'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+    
+    if ($enteredPassword === $user['password']) {
+        
+        $_SESSION['username'] = $enteredUsername;
+        header("Location: homepage.HTML");
+        exit();
+    }
+}
+
+
+?>
